12696d0c88ebe712b5eb3fe41e947194a275e722
Ramesh Sampath <ramesh.sampath@fivestars.com>
2015-03-11 18:09:43 -0700

    update to project structure

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -1,11 +1,17 @@
 import sqlite3
-from flask import Flask
-import pandas as pd
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
-db = sqlite3.connect('../../db/datacanvas.db')
+db = sqlite3.connect('db/datacanvas.db')
 
-@app.route('/metric/<metric>/')
-def metric_data():
-    sql_query = 'select city f
-    df = pd.read_sql('select () from metrics group by ')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# @app.route('/metric/<metric>/')
+# def metric_data():
+#     sql_query = 'select city f
+#     df = pd.read_sql('select () from metrics group by ')
